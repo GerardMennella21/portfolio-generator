362e262df8c6447cb94a80c4714e0884eb970ce7
Gerard Mennella <gerardmennella21@gmail.com>
2022-04-08 06:00:32 -0400

    first question

--- src/page-template.js
+++ src/page-template.js
@@ -2,7 +2,7 @@ module.exports = (name, github) => {
     return `
     <!DOCTYPE html>
     <html lang="en">
-    
+
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width-device-width, initial-scale=1.0">
